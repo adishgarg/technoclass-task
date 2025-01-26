fd4da73310a97e5dcf2fc955401d5e57163645f1
adishgarg <gargadi456@gmail.com>
2025-01-26 20:31:25 +0530

    dynamic import

--- src/app/containers/testimonial.tsx
+++ src/app/containers/testimonial.tsx
@@ -1,9 +1,11 @@
 "use client";
-
+import dynamic from "next/dynamic";
 import React from "react";
-import Slider from "react-slick";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+const Slider = dynamic(() => import("react-slick"),{ssr:false});
 
 export const Testimonial = () => {
   const testimonials = [
