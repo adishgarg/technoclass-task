8a47c3a5caa64e99915d7ae7122732fb93d2b3e5
adishgarg <gargadi456@gmail.com>
2025-01-27 16:29:21 +0530

    dimaag kharab ho geya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+'use client';
+
 import Landing from "./containers/home";
 import Content from "./containers/content";
 // import { Carou } from "./components/carousel";
